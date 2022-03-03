355dce4554aa2acad0363f5c035cbfaf53299874
Lior-Yosef <lioryosef06@gmail.com>
2022-03-03 12:30:09 +0200

    add new changes

--- client/src/service/User-service.js
+++ client/src/service/User-service.js
@@ -1,8 +1,12 @@
 
-const BasicUrl = "http://localhost:3005/User";
+const BasicUrl = process.env.NODE_ENV === 'production' ?
+    'https://office-mern-appliction.herokuapp.com/User' :
+    'http://localhost:3005/User';
 
-export const GetAllUser = () =>{
+
+
+export const GetAllUser = () => {
     return fetch(BasicUrl)
-    .then(res => res.json())
-    .catch(err => console.log(err))
+        .then(res => res.json())
+        .catch(error => console.log(error))
 }
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3005;
 // const port =3005;
 
 
-app.use(express.json({ extended: true}));
+app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
@@ -20,12 +20,16 @@ app.use(cors());
 require('./config/passport')(passport); // ייבוא של הפונקציה מתיקית קונפיג והעברה בתור ארגומנט את מודל הפספורט
 app.use(passport.initialize()) // איתחול של פונקציית הפספורט ומעכשיו הוא בהאזנה כלומר בודק את הטוקן
 
-app.use("/office", passport.authenticate('jwt', {session:false}),OfficeRoutes ); //  זו פונקציה שיודעת לאמת את הטוקן -authenticate
+app.use("/office", passport.authenticate('jwt', { session: false }), OfficeRoutes); //  זו פונקציה שיודעת לאמת את הטוקן -authenticate
 app.use("/User", UserRouter);
 app.get("/", (req, res) => {
     res.send({ massage: "server connected" })
 });
 
-app.listen(port, () => {
-    console.log("lissiom");
-})
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+}
